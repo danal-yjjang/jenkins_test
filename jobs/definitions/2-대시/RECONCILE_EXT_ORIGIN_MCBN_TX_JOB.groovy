941d2d971872401c54868eac33762d2dfd3b9215
yjjang <yjjang@danal.co.kr>
2025-05-20 15:36:21 +0900

    job 추가

---
+++ jobs/definitions/2-대시/RECONCILE_EXT_ORIGIN_MCBN_TX_JOB.groovy
@@ -0,0 +1,27 @@
+import templates.BatchJobTemplate
+
+BatchJobTemplate.job(this, [
+    name : '2-원천사거래대사-20-상품권(RECONCILE_EXT_ORIGIN_MCBN_TX_JOB)', 
+    jobName : 'RECONCILE_EXT_ORIGIN_MCBN_TX_JOB', 
+    parameters : [
+        [
+            type : 'choice', 
+            name : 'serviceCode',
+            description : '서비스코드', 
+            choices : ['BOOK_AND_LIFE', 'HAPPY_MONEY', 'CULTURE_LAND', 'CULTURE_GIFT']
+        ], 
+        [
+            type : 'choice', 
+            name : 'originalCompanyInstitutionCode',
+            description : '원천사기관코드', 
+            choices : ['BOOK_AND_LIFE', 'HAPPY_MONEY', 'CULTURE_LAND', 'CULTURE_GIFT']
+        ], 
+        [
+            type : 'string', 
+            name : 'originalCompanyInstitutionCode'
+            description : '날짜'
+        ]
+    ], 
+    view : '2-대사'
+])
+
