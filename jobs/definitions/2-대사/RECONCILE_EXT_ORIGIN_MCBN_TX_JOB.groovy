540c7dc7e7943991947af1d108b42fd6bff72c48
yjjang <yjjang@danal.co.kr>
2025-05-21 20:40:24 +0900

    test

--- jobs/definitions/2-대사/RECONCILE_EXT_ORIGIN_MCBN_TX_JOB.groovy
+++ jobs/definitions/2-대사/RECONCILE_EXT_ORIGIN_MCBN_TX_JOB.groovy
@@ -1,4 +1,4 @@
-def template = evaluate(readFileFromWorkspace('jobs/atemplates/BatchJobTemplate.groovy'))
+def template = evaluate(readFileFromWorkspace('jobs/templates/BatchJobTemplate.groovy'))
 
 template.job(this, [
     name : '(test) 2-원천사거래대사-20-상품권(RECONCILE_EXT_ORIGIN_MCBN_TX_JOB)', 
