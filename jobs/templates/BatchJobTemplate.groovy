b06d1d87507745f581512b9237a0816a9764fe22
yjjang <yjjang@danal.co.kr>
2025-05-30 17:21:48 +0900

    test

--- jobs/templates/BatchJobTemplate.groovy
+++ jobs/templates/BatchJobTemplate.groovy
@@ -16,9 +16,9 @@ echo ">>>>>>>>>>>>>>>> 최대 수의 디렉터리 : \$max_dir....."
 PARAMS = ""
 ${paramScript}
 
-echo PARAMS
+echo "$PARAMS"
 
-java -Xms256m -Xmx1G -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/home/logs/ -XX:MaxMetaspaceSize=512m -jar /home/service/smart-settlement-batch/\${max_dir}/*.jar --job.name=${config.jobName}PARAMS
+java -Xms256m -Xmx1G -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/home/logs/ -XX:MaxMetaspaceSize=512m -jar /home/service/smart-settlement-batch/\${max_dir}/*.jar --job.name=${config.jobName}\${PARAMS}
 
 exitCodeJava=\$?
 echo ">>>>>>>>>>>>>>>> 프로세스를 종료합니다 - \$exitCodeJava"
