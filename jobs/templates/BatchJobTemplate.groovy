e11115dc405bd9e7ca9bb70d60dd76bfffacc112
yjjang <yjjang@danal.co.kr>
2025-06-09 10:30:52 +0900

    env test5

--- jobs/templates/BatchJobTemplate.groovy
+++ jobs/templates/BatchJobTemplate.groovy
@@ -8,7 +8,7 @@ def job (dslFactory, Map config) {
     }
   }
 
-  def java_options = config.javaOption ?: "\${env.JAVA_DEFAULT_OPTION}"
+  def java_options = config.javaOption ?: env.JAVA_DEFAULT_OPTION
 
   def batchExecCommand = """
 cd /home/service/smart-settlement-batch
