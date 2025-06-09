f30d7eaba3f8e55c928fbc7d8b2c0372858a24e2
yjjang <yjjang@danal.co.kr>
2025-06-09 10:26:41 +0900

    env test4

--- jobs/templates/BatchJobTemplate.groovy
+++ jobs/templates/BatchJobTemplate.groovy
@@ -9,7 +9,7 @@ def job (dslFactory, Map config) {
   }
 
   def env = manager.build.getEnvironment(manager.listener)
-  def java_options = config.javaOption ?: (env['JAVA_DEFAULT_OPTION'] ?: "-Xms256m -Xmx1G -XX:MaxMetaspaceSize=512m")
+  def java_options = config.javaOption ?: "\${env.JAVA_DEFAULT_OPTION}"
 
   def batchExecCommand = """
 cd /home/service/smart-settlement-batch
